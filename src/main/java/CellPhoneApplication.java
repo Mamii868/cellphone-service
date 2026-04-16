97ac678c37e87d388afe2823f5d6f2cd08b07a94
Mamii868 <marcmotenii@gmail.com>
2026-04-16 14:45:21 -0500

    Add user inputs

--- src/main/java/CellPhoneApplication.java
+++ src/main/java/CellPhoneApplication.java
@@ -5,11 +5,21 @@ public class CellPhoneApplication {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.print("What is the serial number? ");
+        int serialNumber =  scanner.nextInt();
+        scanner.nextLine();
+        String model = userStringInput("What model is the phone? ");
+        String carrier = userStringInput("Who is the carrier? ");
+        String phoneNumber = userStringInput("What is the phone number? ");
+        String owner = userStringInput("Who is the owner of the phone? ");
+
+
 
     }
 
     public static String userStringInput(String message) {
-        System.out.println(message);
+        System.out.print(message);
         return scanner.nextLine();
     }
+
 }
