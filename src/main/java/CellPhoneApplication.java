3c384e35706e3252c2941674966ba72cf310a549
Mamii868 <marcmotenii@gmail.com>
2026-04-16 14:41:08 -0500

    Add string input method

--- src/main/java/CellPhoneApplication.java
+++ src/main/java/CellPhoneApplication.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+    }
+
+    public static String userStringInput(String message) {
+        System.out.println(message);
+        return scanner.nextLine();
     }
 }
